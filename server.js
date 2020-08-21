ed7ebe8fdbc2a5eddb4f8a2b2593620148c344e8
Rohit Tiwari <rohittiwarirvt@gmail.com>
2020-08-21 10:47:46 +0530

    Added simple crud operation routes for bootcamp

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ const app = express();
 
 PORT = process.env.PORT || 5000;
 
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get All Bootcamps` });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Get Bootcamp with id ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(201).json({ success: true, msg: `Create A Bootcamp` });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update Bootcamp with id ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp with id ${req.params.id}` });
+});
 app.listen(
   PORT,
   console.log(`Server started for ${process.env.NODE_ENV} Environment for localhost on PORT ${PORT}`)
