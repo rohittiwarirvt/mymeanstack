acf1e0f1576f1eb8af79378bcce640c55faca37f
Rohit Tiwari <rohittiwarirvt@gmail.com>
2021-04-23 16:59:20 +0530

    Added console log

--- server.js
+++ server.js
@@ -43,3 +43,10 @@ process.on('unhandlesRejectionPromise', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
